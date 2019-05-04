49e68e6e2d1dbc7e34da3f6e3761ce315243c1a0
leenorris <lee.norris@philips.com>
2019-05-04 11:52:13 -0400

    blah

--- resume/src/backend/Clients.ts
+++ resume/src/backend/Clients.ts
@@ -1,5 +1,4 @@
 import {IClient} from "src/types/IClient";
-import {endTimeRange} from "@angular/core/src/profile/wtf_impl";
 
 const PA = 'PA';
 const SENIOR_SOFTWARE_ENGINEER = 'Senior Software Engineer';
@@ -9,6 +8,7 @@ const endDate = toDate.bind(null, 'end');
 const PGH = 'Pittsburgh';
 
 export class Clients {
+
     static RIVERS_AGILE_SOLUTIONS: IClient = {
         state: PA,
         name: 'Rivers Agile Solutions',
@@ -18,11 +18,11 @@ export class Clients {
         city: 'Canonsburg',
         engagements: [
             {
-                title: SENIOR_SOFTWARE_ENGINEER,
                 start: startDate(2016, 3),
                 end: endDate(2016, 7),
+                title: SENIOR_SOFTWARE_ENGINEER,
+            }
         ]
-
     };
 
     static PHILIPS_RESPIRONICS: IClient = {
@@ -32,15 +32,32 @@ export class Clients {
         city: 'Murrysville',
         zip: 15668,
         zipExtension: 8517,
-        engagements: [{
-            start: startDate(2016, 7),
-            end: endDate(2019, 8),
-            title: SENIOR_SOFTWARE_ENGINEER
-        }]
+        engagements: [
+            {
+                start: startDate(2015, 7),
+                end: endDate(2016, 3),
+                title: SENIOR_SOFTWARE_ENGINEER
+            },
+            {
+                start: startDate(2016, 7),
+                end: endDate(2019, 8),
+                title: SENIOR_SOFTWARE_ENGINEER
+            }
+        ]
     };
 
-    static IAT_TECHNOLOGIES: IClient = {};
-    static PENN_STATE_UNIVERSITY: IClient = {};
+    static IAT_TECHNOLOGIES: IClient = {
+        state: PA,
+        name: 'IAT Technologies',
+        addressLineOne: '220 Bessemer Rd',
+        city: 'Mount Pleasant',
+        zip: 15666,
+        engagements: [{
+            start: startDate(2011, 3),
+            end: endDate(2012, 3),
+            title: 'Junior Developer'
+        }]
+    };
 
     static UPMC_TECHNOLOGY_DEVELOPMENT_CENTER: IClient = {
         state: PA,
@@ -52,7 +69,7 @@ export class Clients {
         engagements: [{
             start: startDate(2014, 6),
             end: endDate(2015, 7),
-            title: SENIOR_SOFTWARE_ENGINEER
+            title: 'Software Developer'
         }]
     };
 
@@ -65,44 +82,34 @@ export class Clients {
             {
                 start: startDate(2012, 4),
                 end: endDate(2014, 5),
-                title: SENIOR_SOFTWARE_ENGINEER
+                title: 'Programmer/Analyst'
             }
-        ]
-    };
-
-    static ART_ENTERPRISES: IClient = {
-        state: PA,
-        name: 'A.R.T. Enterprises',
-        addressLineOne: 'PO Box 182',
-        city: 'Tarentum',
-        zip: 15084,
-        engagements: [
-
         ]
     };
 
     static FOUR_C_TECHNOLOGIES: IClient = {
         state: PA,
         city: PGH,
-        name: '4CTechnologies',
+        name: '4C Technologies',
         addressLineOne: '1500 Ardmore Blvd',
         addressLineTwo: 'Suite 100',
         zip: 15221,
         engagements: [{
-            start: startDate(2017, 4),
             end: endDate(2017, 6),
-            title: CONSULTANT
+            start: startDate(2017, 4),
+            title: CONSULTANT,
         }]
     };
 
     static getClients (): IClient[] {
         return [
-            Clients.RIVERS_AGILE_SOLUTIONS,
-            Clients.PHILIPS_RESPIRONICS,
-            Clients.UPMC_TECHNOLOGY_DEVELOPMENT_CENTER,
+            Clients.IAT_TECHNOLOGIES,
             Clients.GIANT_EAGLE,
-            // Clients.ART_ENTERPRISES,
-            // Clients.FOUR_C_TECHNOLOGIES,
+            Clients.UPMC_TECHNOLOGY_DEVELOPMENT_CENTER,
+            Clients.PHILIPS_RESPIRONICS,
+            Clients.RIVERS_AGILE_SOLUTIONS,
+            Clients.FOUR_C_TECHNOLOGIES,
+            // Clients.PHILIPS_RESPIRONICS,
         ];
     }
 }
--- resume/src/backend/database.ts
+++ resume/src/backend/database.ts
@@ -1,52 +1,20 @@
 import {ITimeline} from "src/types/ITimeline";
 import {IEngagement} from "src/types/IEngagement";
 import {IClient} from "src/types/IClient";
+import {Clients} from "./Clients";
 
 export class Database {
 
     getEngagements (): IEngagement[] {
         throw new Error("Method not implemented.");
     }
+
     timeline: ITimeline;
     engagements: IEngagement[];
     clients: IClient[];
 
     constructor () {
         this.engagements = this.getEngagements();
-        this.clients = this.getClients();
+        this.clients = Clients.getClients();
     }
 }
-
-
-
-
-
-
-
-Employment Duration2 yrs 11 mos
-Rivers Agile Solutions
-Software Engineer
-Company NameRivers Agile Solutions
-Dates EmployedMar 2016 – Jul 2016
-Employment Duration5 mos
-LocationCanonsburg, PA
- Philips Respironics
-Senior Software Engineer
-Company NamePhilips Respironics
-Dates EmployedJul 2015 – Mar 2016
-Employment Duration9 mos
-LocationMurrysville, PA
-AngularJs, NodeJs, & Java
-
- UPMC Technology Development Center
-Web Developer
-Company NameUPMC Technology Development Center
-Dates EmployedJun 2014 – Jul 2015
-Employment Duration1 yr 2 mos
-LocationBakery Square
-Developing responsive web app with AngularJs and Ruby backend
-
-
-4CTechnologies
-1500 Ardmore Blvd. Suite 100
-Pittsburgh, PA 15221
