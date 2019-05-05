38a6e9c8f8d6e0fef305a457ab3279e3259ef278
leenorris <lee.norris@philips.com>
2019-05-05 09:46:44 -0400

    failing test for invoking engagement comp

--- resume/src/app/chrono-job-list/chrono-job-list.component.spec.ts
+++ resume/src/app/chrono-job-list/chrono-job-list.component.spec.ts
@@ -2,10 +2,15 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ChronoJobListComponent} from './chrono-job-list.component';
 import {Mocks} from 'src/test-helpers/Mocks';
+import {IEngagement} from 'src/types/IEngagement';
+import {Assertions} from 'src/test-helpers/Assertions';
+import {asElementData} from '@angular/core/src/view';
 
 describe('ChronoJobListComponent', () => {
   let component: ChronoJobListComponent;
   let fixture: ComponentFixture<ChronoJobListComponent>;
+  let sampleEngagements: IEngagement[];
+  let assertions: Assertions;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,9 +22,11 @@ describe('ChronoJobListComponent', () => {
   describe('GIVEN: a list of IEngagements are defined on the component', () => {
 
     beforeEach(() => {
+      sampleEngagements = Mocks.getSampleEngagements();
       fixture = TestBed.createComponent(ChronoJobListComponent);
       component = fixture.componentInstance;
-      component.engagements = Mocks.getSampleEngagements();
+      component.engagements = sampleEngagements;
+      assertions = new Assertions(fixture);
       fixture.detectChanges();
     });
 
@@ -27,9 +34,10 @@ describe('ChronoJobListComponent', () => {
       expect(component).toBeTruthy();
     });
 
-
     it('should render one EngagementComponent per IEngagement', () => {
-
+      sampleEngagements.forEach(eng => {
+        assertions.verifyOuterHtml({selector: '.engagement', content: '<app-engagement'});
+      });
     });
   });
 });
